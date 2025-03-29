4337f1dde5193ffb5febb6d553f003451f419672
Anton Src <anton.source@gmail.com>
2025-03-29 20:42:42 +0300

    [add] Add instnce of tree models

--- src/index.js
+++ src/index.js
@@ -8,13 +8,26 @@ import "@babylonjs/inspector";
 const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas, true, { stencil: false });
 
+const MAP_SIZE = 1_000;
+
 window.addEventListener("resize", () => {
   engine.resize();
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.altKey) {
+    if (scene.debugLayer.isVisible()) {
+      scene.debugLayer.hide();
+    } else {
+      scene.debugLayer.show({ overlay: true });
+    }
+  }
 });
 
 function createScene() {
   const scene = new BABYLON.Scene(engine);
 
+  // camera
   const camera = new BABYLON.ArcRotateCamera(
     "camera",
     Math.PI / 2,
@@ -25,8 +38,9 @@ function createScene() {
   );
   camera.attachControl(canvas, true);
   camera.lowerRadiusLimit = 5;
-  camera.upperRadiusLimit = 40;
+  camera.upperRadiusLimit = 500;
 
+  // light
   const light = new BABYLON.HemisphericLight(
     "light",
     new BABYLON.Vector3(0, 1, 0),
@@ -35,9 +49,10 @@ function createScene() {
   light.intensity = 1;
   light.specular = new BABYLON.Color3(0, 0, 0);
 
+  // ground
   const ground = BABYLON.MeshBuilder.CreateGround(
     "ground",
-    { width: 1000, height: 1000 },
+    { width: MAP_SIZE, height: MAP_SIZE },
     scene
   );
   const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
@@ -46,38 +61,92 @@ function createScene() {
   groundTexture.uScale = 20;
   groundTexture.vScale = 20;
   groundMaterial.diffuseTexture = groundTexture;
+  // groundMaterial.wireframe = true
   ground.material = groundMaterial;
 
+  // cubes model
   const cubes = BABYLON.LoadAssetContainerAsync("./cubes.gltf", scene)
     .then((container) => {
       const [root] = container.meshes;
-      root.scaling.setAll(2);
-      root.position._x = 10;
+      root.scaling.setAll(1);
+      root.position.x = 10;
       container.addAllToScene();
     })
     .catch((error) => {
       console.error("Error loading model:", error);
     });
 
+  // tree model
+  const tree = BABYLON.LoadAssetContainerAsync("./tree.glb", scene)
+    .then((container) => {
+      const [root] = container.meshes;
+      root.scaling.setAll(1);
+      root.position.x = -10;
+      // container.addAllToScene();
+
+      const childMeshes = root.getChildMeshes(false);
+      const merged = BABYLON.Mesh.MergeMeshes(
+        childMeshes,
+        true,
+        true,
+        undefined,
+        false,
+        false
+      );
+      merged.isPickable = false;
+      merged.checkCollisions = false;
+
+      const COUNT = 2_000;
+      const offset = 10;
+      const max = MAP_SIZE / 2 - 2 - offset;
+
+      const getPos = () =>
+        (offset + Math.random() * max) * (Math.random() > 0.5 ? 1 : -1);
+
+      for (let i = 0; i < COUNT; i++) {
+        const clone = merged.createInstance("inst_" + i);
+        const x = getPos();
+        const z = getPos();
+        clone.position.set(x, 0, z);
+        clone.rotate(root.up, BABYLON.Scalar.RandomRange(1, 180));
+        clone.scaling.setAll(BABYLON.Scalar.RandomRange(0.5, 2));
+        clone.freezeWorldMatrix();
+        clone.material.freeze();
+        clone.alwaysSelectAsActiveMesh = true;
+      }
+
+      // const bufferMatrices = new Float32Array(16 * COUNT);
+      // for (let i = 0; i < COUNT; i++) {
+      //   const x = getPos();
+      //   const z = getPos();
+      //   // origin.x = x;
+      //   // origin.z = z;
+      //   // const result = scene.pickWithRay(ray, (mesh) => mesh === ground);
+      //   // const y = result.pickedPoint?.y ?? 0;
+      //   const pos = new BABYLON.Vector3(x, 0, z);
+      //   const scale = BABYLON.Vector3.One().setAll(
+      //     BABYLON.Scalar.RandomRange(2, 10)
+      //   );
+      //   const angle = BABYLON.Scalar.RandomRange(0, 2 * Math.PI);
+      //   const rot = BABYLON.Quaternion.FromEulerAngles(0, angle, 0);
+      //   const matrix = BABYLON.Matrix.Compose(scale, rot, pos);
+      //   matrix.copyToArray(bufferMatrices, i * 16);
+      // }
+      // merged.thinInstanceSetBuffer("matrix", bufferMatrices, 16, true);
+    })
+    .catch((error) => {
+      console.error("Error loading model:", error);
+    });
+
+  // cat model
   const cat = BABYLON.LoadAssetContainerAsync("./cat.glb", scene)
     .then((container) => {
       const [root] = container.meshes;
       root.scaling.setAll(1);
       container.addAllToScene();
-
-      const playIdle = () => {
-        container.animationGroups
-          .find((group) => group.name === "idle")
-          .start(true);
-      };
-
-      const playWalk = () => {
-        container.animationGroups.find((group) => group.name === "walk").play();
-      };
-
-      playIdle();
-
       root.rotationQuaternion = BABYLON.Quaternion.Identity();
+
+      playIdle(container);
 
       const targetPoint = root.position.clone();
       const targetRotation = root.rotationQuaternion.clone();
@@ -87,44 +156,7 @@ function createScene() {
       const rotAmount = 4;
       const maxDelta = speed * 0.01;
 
-      scene.onPointerObservable.add((eventData) => {
-        if (eventData.type != BABYLON.PointerEventTypes.POINTERPICK) {
-          return;
-        }
-
-        const pickedMesh = eventData.pickInfo?.pickedMesh;
-        console.log(pickedMesh);
-
-        if (pickedMesh == null) {
-          return;
-        }
-
-        if (pickedMesh.name !== "ground") {
-          return;
-        }
-
-        const pickedPoint = eventData.pickInfo?.pickedPoint;
-
-        if (pickedPoint == null) {
-          return;
-        }
-
-        targetPoint.copyFrom(pickedPoint);
-
-        const dir = targetPoint.subtract(root.position).normalize();
-        targetRotation.copyFrom(
-          BABYLON.Quaternion.FromLookDirectionLH(dir, BABYLON.Vector3.Up())
-        );
-      });
-
       const pressedKeys = {};
-      const keys = {
-        KeyW: -1,
-        KeyS: 1,
-        KeyA: -1,
-        KeyD: 1,
-      };
-
       const axis = {
         forward: 0,
         right: 0,
@@ -133,20 +165,14 @@ function createScene() {
       scene.onKeyboardObservable.add((eventData) => {
         const code = eventData.event.code;
 
-        const getKey = (c) => {
-          return !!pressedKeys[c] ? keys[c] : 0;
-        };
-
         if (eventData.type === BABYLON.KeyboardEventTypes.KEYDOWN) {
-          pressedKeys[code] = 1;
+          pressedKeys[code] = true;
         } else if (eventData.type === BABYLON.KeyboardEventTypes.KEYUP) {
-          pressedKeys[code] = 0;
+          pressedKeys[code] = false;
         }
 
-        axis.forward = getKey("KeyW") + getKey("KeyS");
-        axis.right = getKey("KeyA") + getKey("KeyD");
-
-        console.log(axis);
+        axis.forward = (pressedKeys.KeyW ? -1 : 0) + (pressedKeys.KeyS ? 1 : 0);
+        axis.right = (pressedKeys.KeyA ? -1 : 0) + (pressedKeys.KeyD ? 1 : 0);
       });
 
       scene.onBeforeRenderObservable.add(() => {
@@ -178,11 +204,11 @@ function createScene() {
         const diff = targetPoint.subtract(root.position);
 
         if (diff.length() < maxDelta) {
-          playIdle();
+          playIdle(container);
           return;
         }
 
-        playWalk();
+        playWalk(container);
 
         const dir = diff.normalize();
         const velocity = dir.scaleInPlace(speed * deltaTime);
@@ -202,12 +228,10 @@ engine.runRenderLoop(() => {
   scene.render();
 });
 
-window.addEventListener("keydown", (event) => {
-  if (event.ctrlKey && event.altKey) {
-    if (scene.debugLayer.isVisible()) {
-      scene.debugLayer.hide();
-    } else {
-      scene.debugLayer.show({ overlay: true });
-    }
-  }
-});
+function playIdle(container) {
+  container.animationGroups.find((group) => group.name === "idle").start(true);
+}
+
+function playWalk(container) {
+  container.animationGroups.find((group) => group.name === "walk").play();
+}
