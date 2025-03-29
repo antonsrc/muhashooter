dc24aa16f85d1f99092b7d714b98a0aec358ca1b
Anton Src <anton.source@gmail.com>
2025-03-29 14:23:59 +0300

    [add] Add controls by mouse and keyboard

--- src/index.js
+++ src/index.js
@@ -41,7 +41,11 @@ function createScene() {
     scene
   );
   const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-  groundMaterial.diffuseColor = new BABYLON.Color3(0.1, 0.3, 0.4);
+  // groundMaterial.diffuseColor = new BABYLON.Color3(0.1, 0.3, 0.4);
+  const groundTexture = new BABYLON.Texture("./grass.jpg", scene);
+  groundTexture.uScale = 20;
+  groundTexture.vScale = 20;
+  groundMaterial.diffuseTexture = groundTexture;
   ground.material = groundMaterial;
 
   const cubes = BABYLON.LoadAssetContainerAsync("./cubes.gltf", scene)
@@ -57,17 +61,133 @@ function createScene() {
 
   const cat = BABYLON.LoadAssetContainerAsync("./cat.glb", scene)
     .then((container) => {
-      container.animationGroups.forEach((ag) => {
-        if (ag.name === "idle") {
-          ag.start = true;
-        } else {
-          ag.stop();
-        }
-      });
-
       const [root] = container.meshes;
       root.scaling.setAll(1);
       container.addAllToScene();
+
+      const playIdle = () => {
+        container.animationGroups
+          .find((group) => group.name === "idle")
+          .start(true);
+      };
+
+      const playWalk = () => {
+        container.animationGroups.find((group) => group.name === "walk").play();
+      };
+
+      playIdle();
+
+      root.rotationQuaternion = BABYLON.Quaternion.Identity();
+
+      const targetPoint = root.position.clone();
+      const targetRotation = root.rotationQuaternion.clone();
+
+      const speed = 10;
+      const rotLerpSpeed = 10;
+      const rotAmount = 4;
+      const maxDelta = speed * 0.01;
+
+      scene.onPointerObservable.add((eventData) => {
+        if (eventData.type != BABYLON.PointerEventTypes.POINTERPICK) {
+          return;
+        }
+
+        const pickedMesh = eventData.pickInfo?.pickedMesh;
+        console.log(pickedMesh);
+
+        if (pickedMesh == null) {
+          return;
+        }
+
+        if (pickedMesh.name !== "ground") {
+          return;
+        }
+
+        const pickedPoint = eventData.pickInfo?.pickedPoint;
+
+        if (pickedPoint == null) {
+          return;
+        }
+
+        targetPoint.copyFrom(pickedPoint);
+
+        const dir = targetPoint.subtract(root.position).normalize();
+        targetRotation.copyFrom(
+          BABYLON.Quaternion.FromLookDirectionLH(dir, BABYLON.Vector3.Up())
+        );
+      });
+
+      const pressedKeys = {};
+      const keys = {
+        KeyW: -1,
+        KeyS: 1,
+        KeyA: -1,
+        KeyD: 1,
+      };
+
+      const axis = {
+        forward: 0,
+        right: 0,
+      };
+
+      scene.onKeyboardObservable.add((eventData) => {
+        const code = eventData.event.code;
+
+        const getKey = (c) => {
+          return !!pressedKeys[c] ? keys[c] : 0;
+        };
+
+        if (eventData.type === BABYLON.KeyboardEventTypes.KEYDOWN) {
+          pressedKeys[code] = 1;
+        } else if (eventData.type === BABYLON.KeyboardEventTypes.KEYUP) {
+          pressedKeys[code] = 0;
+        }
+
+        axis.forward = getKey("KeyW") + getKey("KeyS");
+        axis.right = getKey("KeyA") + getKey("KeyD");
+
+        console.log(axis);
+      });
+
+      scene.onBeforeRenderObservable.add(() => {
+        const deltaTime = (scene.deltaTime ?? 1) / 1000;
+
+        if (Math.abs(axis.forward) > 0.001) {
+          const nextPoint = root.position.add(
+            root.forward.scale(axis.forward * 0.3)
+          );
+          targetPoint.copyFrom(nextPoint);
+        }
+
+        if (Math.abs(axis.right) > 0.001) {
+          targetRotation.multiplyInPlace(
+            BABYLON.Quaternion.RotationAxis(
+              root.up,
+              axis.right * rotAmount * deltaTime
+            )
+          );
+        }
+
+        BABYLON.Quaternion.SlerpToRef(
+          root.rotationQuaternion,
+          targetRotation,
+          rotLerpSpeed * deltaTime,
+          root.rotationQuaternion
+        );
+
+        const diff = targetPoint.subtract(root.position);
+
+        if (diff.length() < maxDelta) {
+          playIdle();
+          return;
+        }
+
+        playWalk();
+
+        const dir = diff.normalize();
+        const velocity = dir.scaleInPlace(speed * deltaTime);
+        root.position.addInPlace(velocity);
+      });
     })
     .catch((error) => {
       console.error("Error loading model:", error);
