20d8472a2d4978980362e0c16b7f50da8a6b5f48
Anton Src <anton.source@gmail.com>
2026-01-12 21:48:58 +0300

    [upd] изменены тени, стал более psx стиль

--- src/shadows.js
+++ src/shadows.js
@@ -1,11 +1,21 @@
 import * as BABYLON from "@babylonjs/core";
 
 export function createShadows(light) {
-  const shadowGenerator = new BABYLON.CascadedShadowGenerator(2048, light);
-  shadowGenerator.cascadeCount = 3;
-  shadowGenerator.lambda = 0.95;
+  const shadowGenerator = new BABYLON.CascadedShadowGenerator(1024, light);
+  shadowGenerator.cascadeCount = 1;
+  shadowGenerator.lambda = 0.5;
   shadowGenerator.quality = BABYLON.ShadowGenerator.QUALITY_LOW;
-  shadowGenerator.bias = 0.001;
-  shadowGenerator.normalBias = 0.01;
+  shadowGenerator.bias = 0.02;
+  shadowGenerator.normalBias = 0.05;
+  shadowGenerator.depthScale = 0;
+
+   // Отключаем все фильтры и размытия
+  shadowGenerator.filteringQuality = BABYLON.ShadowGenerator.QUALITY_LOW;
+  shadowGenerator.usePercentageCloserFiltering = false;
+  shadowGenerator.useContactHardeningShadow = false;
+  
+  // Настройки для резких, пиксельных краев
+  shadowGenerator.frustumEdgeFalloff = 0; // Резкие края теней
+  shadowGenerator.shadowMaxZ = 100; // Увеличиваем дальность теней
   return shadowGenerator;
 }
